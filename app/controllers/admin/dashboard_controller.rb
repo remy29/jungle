c9dd40f1dfd879aef63168cd266741a8474e2a5c
remy29 <arthur.d.remy@gmail.com>
2021-01-07 08:02:32 +0000

    added product stats chart to admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+
   def show
+    @categories = Category.all
+    @products = Product.all 
   end
+
 end
